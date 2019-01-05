c2c82b1cd0c813b093b4e266486c119733bf6de6
Andrew Duffy <agd@cs.stanford.edu>
2019-01-05 18:42:33 -0500

    add write bench

--- src/main/java/io/github/a10y/bench/UncontendedMemoryReads.java
+++ src/main/java/io/github/a10y/bench/UncontendedMemoryReads.java
@@ -9,6 +9,7 @@ import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
@@ -17,21 +18,35 @@ import org.openjdk.jmh.annotations.Warmup;
  *
  * Results on a 2017 rMBP:
  *
- * Benchmark                                  Mode  Cnt  Score   Error  Units
- * UncontendedMemoryReads.bench_readAtomic    avgt    3  2.217 ± 0.597  ns/op
- * UncontendedMemoryReads.bench_readDirect    avgt    3  2.192 ± 0.607  ns/op
- * UncontendedMemoryReads.bench_readVolatile  avgt    3  2.159 ± 0.117  ns/op
+ * Benchmark                                   Mode  Cnt   Score   Error  Units
+ * UncontendedMemoryReads.bench_readAtomic     avgt    3   2.217 ± 0.597  ns/op
+ * UncontendedMemoryReads.bench_readDirect     avgt    3   2.192 ± 0.607  ns/op
+ * UncontendedMemoryReads.bench_readVolatile   avgt    3   2.159 ± 0.117  ns/op
+ * UncontendedMemoryReads.bench_writeAtomic    avgt    3   7.091 ± 0.652  ns/op
+ * UncontendedMemoryReads.bench_writeDirect    avgt    3   2.228 ± 0.519  ns/op
+ * UncontendedMemoryReads.bench_writeVolatile  avgt    3  10.715 ± 3.584  ns/op
  */
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Fork(1)
+@Fork(
+        value = 1,
+        jvmArgsAppend = {"-XX:+UnlockDiagnosticVMOptions",
+                         "-XX:PrintAssemblyOptions=intel",
+                         "-XX:CompileCommand=print,*UncontendedMemoryReads.bench_*"})
 @Measurement(iterations = 3, time = 10)
 @Warmup(iterations = 3, time = 4)
 public class UncontendedMemoryReads {
     volatile int xVol = 0xFFFF00CC; // whatevs
     int x = 0xFFFF00CC; // whatevs
     AtomicInteger xAtomic = new AtomicInteger(0xFFFF00CC);
+
+    @Setup
+    public void setup() {
+        xVol = 0xFFFF00CC;
+        x = 0xFFFF00CC;
+        xAtomic.set(0xFFFF00CC);
+    }
 
     @Benchmark
     public int bench_readVolatile() {
@@ -46,5 +61,22 @@ public class UncontendedMemoryReads {
     @Benchmark
     public int bench_readAtomic() {
         return xAtomic.get() + 1;
+    }
+
+    @Benchmark
+    public int bench_writeVolatile() {
+        xVol += 1;
+        return xVol;
+    }
+
+    @Benchmark
+    public int bench_writeDirect() {
+        x += 1;
+        return x;
+    }
+
+    @Benchmark
+    public int bench_writeAtomic() {
+        return xAtomic.incrementAndGet();
     }
 }
